72df096b9d989ad87857e3781ebbb8531864319d
VictorDemerji <demerji.v1@gmail.com>
2025-11-20 18:01:42 +0200

    Initial commit

---
+++ src/main/java/pixel/academy/antrenor/AntrenorApplication.java
@@ -0,0 +1,13 @@
+package pixel.academy.antrenor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntrenorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntrenorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pixel/academy/antrenor/AntrenorApplicationTests.java
@@ -0,0 +1,13 @@
+package pixel.academy.antrenor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntrenorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
